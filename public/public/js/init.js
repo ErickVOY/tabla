30759218b9e7ed155f779b8526838225e37902c9
ErickVOY <37811991+ErickVOY@users.noreply.github.com>
2018-03-26 14:22:03 -0600

    iniciaizacion

---
+++ public/public/js/init.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAi7xq0vaZq9XU0FyEipoXXxv6P0KoiuJI",
+  authDomain: "tablaperiodica-8af01.firebaseapp.com",
+  databaseURL: "https://tablaperiodica-8af01.firebaseio.com",
+  projectId: "tablaperiodica-8af01",
+  storageBucket: "tablaperiodica-8af01.appspot.com",
+  messagingSenderId: "376808640828"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
