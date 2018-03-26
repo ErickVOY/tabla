ac3c31bf827ad7afe6812eeac7f7a864a8a53604
Antonio Mendoza <antonio.mendoza.mmg@gmail.com>
2018-03-26 14:56:53 -0600

    new

--- public/public/js/actions.js
+++ public/public/js/actions.js
@@ -1,3 +1,13 @@
 $( function () {
-	
+	$('#regElement').on('click', function () {
+		var numeroAtomico = $('input[name="numeroAtomico"]').val();
+		var masaAtomica = $('input[name="masaAtomica"]').val();
+		var energiaDeIonizacion = $('input[name="energiaDeIonizacion"]').val();
+		var SimboloQuimico = $('input[name="SimboloQuimico"]').val();
+		var nombre = $('input[name="nombre"]').val();
+		var configuracionElectronica = $('input[name="configuracionElectronica"]').val();
+		var electronegatividad = $('input[name="electronegatividad"]').val();
+		var estadosDeOxidacion = $('input[name="estadosDeOxidacion"]').val();
+		var TipoDeElemento = $('input[name="TipoDeElemento"]').val();
+	});
 });
